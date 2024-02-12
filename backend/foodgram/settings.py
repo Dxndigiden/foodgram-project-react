e59cf13eaeb0d70c1e8d513779b227ab3f7bd78e
Денис Смирнов <deadpoolair@gmail.com>
2024-02-12 23:37:17 +0700

    deploy test4/4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -67,8 +67,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
