a483d9c4e7ac74c59822019009ba822734837145
Денис Смирнов <deadpoolair@gmail.com>
2024-04-07 22:35:43 +0700

    rev last

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -20,7 +20,7 @@ class Ingredient(models.Model):
     class Meta:
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
-        ordering = ('name',)
+        ordering = ['name']
         constraints = [
             models.UniqueConstraint(fields=['name', 'measurement_unit'],
                                     name='unique_together')
