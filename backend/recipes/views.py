815fb94fe91fa608631688ddad36586d1bf2a034
Денис Смирнов <deadpoolair@gmail.com>
2024-04-11 00:22:36 +0700

    fix recip5

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,8 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from recipes.models import (Ingredient, IngredientInRecipe,
                             Recipe, Tag)
 from rest_framework.decorators import action
-from rest_framework.permissions import (SAFE_METHODS,
-                                        IsAuthenticated,
+from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_400_BAD_REQUEST,
